1a68319610484c5692b2995515495a82c7c4a228
Ninjabot10000 <ninjabot10000@gmail.com>
2022-01-21 15:14:48 -0800

    added some javascript. Still working on it

---
+++ javascript/javascript.js
@@ -0,0 +1,24 @@
+function newMovie() {
+    let movie = new movie(
+        document.getElementById("title").value,
+        document.getElementById("rating").value,
+    );
+
+    if (!movie.isValid()) {
+        alert("Invalid data input. Please enter the correct data.");
+    }
+     else {
+        movie.push(movie); 
+        // displayMovie();
+    }
+    //Removes anything in the entry boxes
+    document.getElementById("title").value = "";
+    document.getElementById("rating").value = "";
+};
+
+let movies = [];
+
+let displayMovie = function(){
+    let  = "";
+
+}
