9ab34b5f8478ad9421ba018e36b94183030a6466
Ninjabot10000 <ninjabot10000@gmail.com>
2022-01-21 17:42:33 -0800

    Added year feature

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,27 +1,10 @@
 
-function movie(title, rating){
-
-    this.title = title;
-    this.rating = rating;
-    this.validate = function(){
-            var num_rat = parseInt(this.rating);
-            
-            if(title && (num_rat>=1 && num_rat<=5)){
-                    return true;
-            }
-            else{ 
-                    return false;
-            }
-    }
-    this.toString = function(){
-            return "Movie: "+this.title+", Rating: "+this.rating;
-    }
-}
 let movies = [];
 function addMovie(){
         let title = document.getElementById('title').value;
         let rating = document.getElementById('rating').value;
-        let newMovie = new movie(title, rating);
+        let year = document.getElementById('year').value;
+        let newMovie = new movie(title, rating, year);
         if(newMovie.validate()){
                 
                 movies.push(newMovie);
@@ -33,6 +16,10 @@ function addMovie(){
         
         document.getElementById('title').value="";
         document.getElementById('rating').value="";
+        document.getElementById('year').value="";
+        movies.sort((a,b) => {
+                return parseFloat(b.rating) - parseFloat(a.rating);
+        });
 }
 
 function showMovies(){
@@ -40,32 +27,37 @@ function showMovies(){
         for(var i=0;i<movies.length;i++){
                 htmlText += "<li>"+movies[i].toString()+"</li>";
         }
-    
         htmlText += "</ul>";
         document.getElementById('display').innerHTML = htmlText;
+
+        
 }
 
-function newMovie() {
-    let movie = new movie(
-        document.getElementById("title").value,
-        document.getElementById("rating").value,
-    );
-    if (!movie.isValid()) {
-        alert("Invalid data input. Please enter the correct data.");
-    }
-     else {
-        movie.push(movie); 
-        // displayMovie();
-    }
-    //Removes anything in the entry boxes
-    document.getElementById("title").value = "";
-    document.getElementById("rating").value = "";
-};
 
-let movies = [];
 
-let displayMovie = function(){
-    let  = "";
 
-}
+
+// function newMovie() {
+//     let movie = new movie(
+//         document.getElementById("title").value,
+//         document.getElementById("rating").value,
+//     );
+//     if (!movie.isValid()) {
+//         alert("Invalid data input. Please enter the correct data.");
+//     }
+//      else {
+//         movie.push(movie); 
+//         // displayMovie();
+//     }
+//     //Removes anything in the entry boxes
+//     document.getElementById("title").value = "";
+//     document.getElementById("rating").value = "";
+// };
+
+// let movies = [];
+
+// let displayMovie = function(){
+//     let  = "";
+
+// }
 
--- javascript/movies.js
+++ javascript/movies.js
@@ -1,3 +1,26 @@
-// let Movies(title, rating){
+function movie(title, rating, year){
 
-// }
+    this.title = title;
+    this.rating = rating;
+    this.year = year;
+    
+    this.validate = function(){
+            let num_rat = parseInt(this.rating);
+            
+            
+            if(title && (num_rat>=1 && num_rat<=5)){
+                    return true;
+            }
+            else{ 
+                    return false;
+            }
+    }
+    
+    this.toString = function(){
+            return "Movie: "+this.title+", Rating: "+this.rating+",Year: "+this.year;
+    }
+    
+}
+
+
+
